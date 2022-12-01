ecc43329941b629e40a05da7ef972118143a0297
Derwes <vip.derwer.vip.2@bk.ru>
2022-12-01 14:15:32 +0300

    Update bd_controller.py

--- sources/back/bd_controller.py
+++ sources/back/bd_controller.py
@@ -6,6 +6,7 @@ DBCur = DBCon.cursor()
 tableCreate = "CREATE TABLE IF NOT EXISTS projectors_info (id INTEGER PRIMARY KEY UNIQUE, type TEXT NOT NULL, group_id INTEGER, dim INTEGER, rgb INTEGER, r INTEGER, g INTEGER, b INTEGER, pan INTEGER, tilt INTEGER, panSpeed INTEGER, tiltSpeed INTEGER, focus INTEGER, zoom INTEGER, shutter INTEGER);"
 DBCur.execute(tableCreate)
 
+# Takes an array of 14 elements.
 def add_projector(data):
     getCount = "SELECT * FROM projectors_info"
     DBCur.execute(getCount)
@@ -17,6 +18,7 @@ def add_projector(data):
     DBCon.commit()
     print(f"Projector with id {cnt} added")
 
+# Takes dmx adress of dimmer channel.
 def remove_projector(dim_adress):
     getCount = "SELECT * FROM projectors_info WHERE dim = ?"
     DBCur.execute(getCount, [dim_adress])
@@ -26,8 +28,6 @@ def remove_projector(dim_adress):
     DBCur.execute(qur, [dim_adress])
     DBCon.commit()
     print(f"{count} projectors were succesfully removed")
-
-add_projector([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 
 DBCur.close()
 DBCon.close()
