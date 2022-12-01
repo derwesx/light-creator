65f9a83a4af5134fc249e24f72be5667778bbde0
Derwes <vip.derwer.vip.2@bk.ru>
2022-12-01 14:34:24 +0300

    Added Color generator
    Color generator

--- sources/back/light_controller.py
+++ sources/back/light_controller.py
@@ -1,11 +1,22 @@
 import threading
 import random
 import time
-import sacn
 
 from sources.models import Config
 from sources.back.consts import *
 
+# Working with color generator
+import numpy as np, cv2
+colorMap = cv2.imread("sources/back/colormap.png")
+def getRandomColor():
+    print("Getting random color...")
+    x = random.randint(0, len(colorMap))
+    y = random.randint(0, len(colorMap[0]))
+    print(f"Got color -> {colorMap[x][y]}")
+    return colorMap[x][y]
+    
+# Working with DMX Signal Sender
+import sacn
 sender = sacn.sACNsender(fps=60)
 sender.start()
 sender.activate_output(1)
@@ -15,4 +26,4 @@ def catchRequest(request):
     print(request)
 
 A = Projector()
-A.switchColor([1, 2, 3])
+A.switchColor(getRandomColor())
