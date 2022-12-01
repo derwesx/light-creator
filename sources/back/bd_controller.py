60a99fb284dfc4e8cba9377579873177440fe3d0
Derwes <vip.derwer.vip.2@bk.ru>
2022-12-01 12:39:25 +0300

    Update bd_controller.py

--- sources/back/bd_controller.py
+++ sources/back/bd_controller.py
@@ -3,18 +3,22 @@ import sqlite3
 DBCon = sqlite3.connect("sources/db.sqlite3")
 DBCur = DBCon.cursor()
 
-DBCur.execute("CREATE TABLE IF NOT EXISTS projectors_info (id INTEGER PRIMARY KEY UNIQUE, type STRING NOT NULL, group_id INTEGER, dim INTEGER, rgb INTEGER, r INTEGER, g INTERGER, b INTEGER, pan INTEGER, tilt INTEGER, panSpeed INTEGER, tiltSpeed INTEGER, focus INTEGER, zoom INTEGER, shutter INTEGER);")
-DBCur.execute("SELECT * FROM projectors_info")
-print(DBCur.fetchall())
-
-def RequestsQueue():
-    pass
-
-def addRequetsToQueue(req):
-    pass
+tableCreate = "CREATE TABLE IF NOT EXISTS projectors_info (id INTEGER PRIMARY KEY UNIQUE, type STRING NOT NULL, group_id INTEGER, dim INTEGER, rgb INTEGER, r INTEGER, g INTERGER, b INTEGER, pan INTEGER, tilt INTEGER, panSpeed INTEGER, tiltSpeed INTEGER, focus INTEGER, zoom INTEGER, shutter INTEGER);"
+DBCur.execute(tableCreate)
 
 def add_projector(data):
-    pass
+    getCount = "SELECT * FROM projectors_info"
+    DBCur.execute(getCount)
+    cnt = len(DBCur.fetchall())
+    print(cnt)
+    qur = "INSERT INTO projectors_info(id, type, group_id, dim, rgb, r, g, b, pan, tilt, panSpeed, tiltSpeed, focus, zoom, shutter) " \
+          "VALUES(%d, %s, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)"
+    print([cnt] + data)
+    DBCur.execute(qur, (0, '1', 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14))
 
 def remove_projector(dim_adress):
     pass
+
+add_projector([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+add_projector([2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+add_projector([3, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
