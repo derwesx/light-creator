545f210b3d54217af85fe4fc998557a4f343c54c
Derwes <vip.derwer.vip.2@bk.ru>
2022-12-10 13:02:05 +0300

    Update person_searcher.py

--- sources/back/person_searcher.py
+++ sources/back/person_searcher.py
@@ -1,52 +1,40 @@
 import numpy as np
 import cv2
-
+ 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cv2.startWindowThread()
-
-# open webcam video stream
 cap = cv2.VideoCapture(0)
 
-# the output will be written to output.avi
-out = cv2.VideoWriter(
-    'output.avi',
-    cv2.VideoWriter_fourcc(*'MJPG'),
-    15.,
-    (640,480))
+def cnv(val, a, b, c, d):
+    return int((val - a) * (d - c) / (b - a) + c)
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
-
-    # resizing for faster detection
-    frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
+ 
+    frame = cv2.resize(frame, (1280, 700))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
-
+ 
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8))
+ 
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
+    x = 0
+    i = 0
+ 
     for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
-    
-    # Write the output video 
-    out.write(frame.astype('uint8'))
-    # Display the resulting frame
+        if weights[i] < 1 or abs(xB-xA) * abs(yB-yA) > 300 * 300:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 0, 255), 2)
+            i+=1
+            continue
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        i+=1
+        x+=1
+        midx = (xA + xB) / 2
+        midy = yA
+ 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything done, release the capture
-cap.release()
-# and release the output
-out.release()
-# finally, close the window
+ 
 cv2.destroyAllWindows()
-cv2.waitKey(1)
